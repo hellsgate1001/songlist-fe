2592629bc0213c4877a47c6ddd81833acf5c5844
Brian Wilson <brianwilson1001@hotmail.com>
2018-10-28 15:12:26 +0000

    Initial commit with draggable song items

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SongList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
+
+import SongList from './components/SongList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SongList
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/SongList.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <md-list>
+      <draggable v-model="songs" @start="drag=true" @stop="drag=false">
+        <md-list-item v-for="song in songs" :key="song">
+          <span class="md-list-item-text">{{song}}</span>
+        </md-list-item>
+      </draggable>
+    </md-list>
+
+  </section>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+
+export default {
+  name: 'SongList',
+  components: {
+      draggable
+  },
+  data() {
+    return {
+      songs: [
+        'Blondie - Call Me',
+        'Alice In Chains - Man In A Box',
+        'Guns N Roses - Nightrain',
+        'Heart - Barracuda',
+        'Alter Bridge - Blackbird',
+        'Green Day - Longview',
+        'Foo Fighters - The Pretender',
+        'Muse - Hysteria',
+        'Black Stone Cherry - Blind Man',
+        'Jane\'s Addiction - Been Caught Stealing'
+      ]
+    }
+  }
+}
+</script>
